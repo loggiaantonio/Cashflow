dadf9b4a8e3dcb4a3e804d3a184402c799df20f4
Antonio Loggia <antonio.loggia@hotmail.de>
2024-08-05 14:05:18 +0200

    setup project erledigt in xcode

---
+++ CashFlow/CashFlow/CashFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashFlowApp.swift
+//  CashFlow
+//
+//  Created by Antonio Loggia on 05.08.24.
+//
+
+import SwiftUI
+
+@main
+struct CashFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
